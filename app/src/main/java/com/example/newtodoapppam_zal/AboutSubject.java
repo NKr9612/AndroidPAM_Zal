004d0a3f037fb2706c2e76aff38c77b28c7c8094
Norbert Krupa <toftnqr@gmail.com>
2021-01-13 11:20:41 +0100

    Adding activities

---
+++ app/src/main/java/com/example/newtodoapppam_zal/AboutSubject.java
@@ -0,0 +1,14 @@
+package com.example.newtodoapppam_zal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutSubject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_subject);
+    }
+}
--- app/src/main/java/com/example/newtodoapppam_zal/MainActivity.java
+++ app/src/main/java/com/example/newtodoapppam_zal/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.newtodoapppam_zal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private Button checkIfLower;
     private Button btnRandom;
     private Button btnToast;
+    private Button aboutApp;
+    private Button aboutAuthor;
+    private Button aboutSubject;
 
 
     @Override
@@ -39,64 +43,103 @@ public class MainActivity extends AppCompatActivity {
         checkIfLower = findViewById(R.id.checkIfLower);
         checkIfLower.setOnClickListener(newLow);
 
+        //--------------aboutApp
+        aboutApp = findViewById(R.id.aboutApp);
+        aboutApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAboutApp();
+            }
+        });
+       //----------------aboutAuthor
+        aboutAuthor = findViewById(R.id.aboutAuthor);
+        aboutAuthor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAboutAuthor();
+            }
+        });
+        //---------------aboutSubject
+        aboutSubject = findViewById(R.id.aboutSubject);
+        aboutSubject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAboutSubject();
+            }
+        });
+
 
     }
-    private View.OnClickListener newLow = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            TextView textArea = (TextView) findViewById(R.id.textArea);
-            String text = (String) textArea.getText();
-            int i = Integer.parseInt(text);
+    private void openAboutSubject() {
+        Intent intentAboutSubject = new Intent(this, AboutSubject.class);
+        startActivity(intentAboutSubject);
+    }
 
-            if(i >= 5){
-                Toast.makeText(MainActivity.this, "It's under 5", Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(MainActivity.this, "Ups. Random other number", Toast.LENGTH_SHORT).show();
+        private void openAboutAuthor () {
+            Intent intentAboutAuthor = new Intent(this, AboutAuthor.class);
+            startActivity(intentAboutAuthor);
+        }
+
+            private void openAboutApp () {
+                Intent intentAboutApp = new Intent(this, AboutApp.class);
+                startActivity(intentAboutApp);
             }
+            private View.OnClickListener newLow = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView textArea = (TextView) findViewById(R.id.textArea);
+                    String text = (String) textArea.getText();
+                    int i = Integer.parseInt(text);
+
+                    if (i >= 5) {
+                        Toast.makeText(MainActivity.this, "It's under 5", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Ups. Random other number", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            };
+
+
+            private View.OnClickListener newHigh = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+
+                    TextView textArea = (TextView) findViewById(R.id.textArea);
+                    String text = (String) textArea.getText();
+                    int i = Integer.parseInt(text);
+
+                    if (i >= 5) {
+                        Toast.makeText(MainActivity.this, "It's bigger than 5", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Ups. Random other number", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            };
+
+            private View.OnClickListener newRandom = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Random rn = new Random();
+                    int answer = rn.nextInt(10) + 1;
+
+                    String answerA = Integer.toString(answer);
+
+                    TextView textArea = (TextView) findViewById(R.id.textArea);
+
+                    textArea.setText(answerA);
+
+
+                }
+            };
+
+
+            private View.OnClickListener newToast = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "Cheers!", Toast.LENGTH_SHORT).show();
+                }
+            };
+
+
         }
-    };
-
-
-    private View.OnClickListener newHigh = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-
-            TextView textArea = (TextView) findViewById(R.id.textArea);
-            String text = (String) textArea.getText();
-            int i = Integer.parseInt(text);
-
-            if(i >= 5){
-                  Toast.makeText(MainActivity.this, "It's bigger than 5", Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(MainActivity.this, "Ups. Random other number", Toast.LENGTH_SHORT).show();
-            }
-        }
-    };
-
-    private View.OnClickListener newRandom = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Random rn = new Random();
-            int answer = rn.nextInt(10) + 1;
-
-            String answerA = Integer.toString(answer);
-
-            TextView textArea = (TextView) findViewById(R.id.textArea);
-
-            textArea.setText(answerA);
-
-
-        }
-    };
-
-
-    private View.OnClickListener newToast = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Toast.makeText(MainActivity.this, "Cheers!", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-
-}
